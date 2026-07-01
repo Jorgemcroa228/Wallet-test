5a1fb8761ac7c0e8bf215aa02dc041fd9de62e3d
jorgemcroa228 <jorgemcroa228@gmail.com>
2026-06-30 21:57:28 -0500

    creating some responseDTO

--- Desktop/Wallet-Test/src/main/java/com/finance/Wallet_Test/dto/response/UsersResponsetDTO.java
+++ Desktop/Wallet-Test/src/main/java/com/finance/Wallet_Test/dto/response/UsersResponsetDTO.java
@@ -1,5 +1,23 @@
 package com.finance.Wallet_Test.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class UsersResponsetDTO {
   
+  private Long IdUser;
+  private String name;
+  private String email;
+  private String document;
+  private String phone;
 }
