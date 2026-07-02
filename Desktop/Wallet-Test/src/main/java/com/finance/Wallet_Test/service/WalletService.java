16a6dad9c00c396ca9b19137b912e67a4afa0665
jorgemcroa228 <jorgemcroa228@gmail.com>
2026-07-01 22:09:38 -0500

    Creating Reacharge in WalletService

--- Desktop/Wallet-Test/src/main/java/com/finance/Wallet_Test/service/WalletService.java
+++ Desktop/Wallet-Test/src/main/java/com/finance/Wallet_Test/service/WalletService.java
@@ -2,7 +2,10 @@ package com.finance.Wallet_Test.service;
 
 import org.springframework.stereotype.Service;
 
+import com.finance.Wallet_Test.dto.request.RechargeRequestDTO;
 import com.finance.Wallet_Test.dto.response.WalletResponseDTO;
+import com.finance.Wallet_Test.entity.UsersEntity;
+import com.finance.Wallet_Test.entity.WalletEntity;
 import com.finance.Wallet_Test.repository.UsersRepository;
 import com.finance.Wallet_Test.repository.WalletRepository;
 
@@ -17,5 +20,12 @@ public class WalletService {
   private final UsersRepository usersRepository;
   private final WalletRepository walletRepository;
 
-  public WalletResponseDTO recharge()
+  public WalletResponseDTO recharge(RechargeRequestDTO request) {
+    UsersEntity user = findUser(request.getDocument(), request.getPhone());
+    WalletEntity wallet = findWallet(user);
+
+    certify (wallet, request.getValor());
+
+    log.info("Succesfull recharge. documento={} valor={}");
+  }
 }
